88f3f792a5f25e09c58bec62ce84bd9b09e7fba7
LazyCow <123736479+nft-dna@users.noreply.github.com>
2023-03-09 01:01:22 +0100

    Update BlessOrBlame.sol
    - major draft update

--- BlessOrBlame.sol
+++ BlessOrBlame.sol
@@ -12,27 +12,25 @@ import "./TokenUriLogicContract.sol";
 
 contract SoulBoundBlessOrBlame is Context, ERC165, IERC1155, IERC1155MetadataURI {
     using Address for address;
-
-    // Mapping from token ID to account balances
+    address public constant RewardContract =  address(0xE8aF6d7e77f5D9953d99822812DCe227551df1D7);
 	mapping(uint16 => mapping(address => uint256)) private _balances;
 	mapping(address => mapping(address => uint16 )) private _issuer;	
 
 	TokenUriLogicContract public TokenUriLogic;
-	mapping(address => mapping(uint16 => uint256)) public Owned;
     uint256 public MintTokenPrice = 50000000000000000; // 0.050
-    address private _owner;
+    address private _creator;
 
     constructor() {
-		_owner = _msgSender();
+		_creator = _msgSender();
     }
 	
     function setTokenUriLogic(address tokenUriLogic) external {
-		require(_owner == _msgSender(), "must be owner");
+		require(_creator == _msgSender(), "must be owner");
         TokenUriLogic = TokenUriLogicContract(tokenUriLogic);
     }
 
     function setMintTokenPrice(uint256 price) external {
-		require(_owner == _msgSender(), "must be owner");
+		require(_creator == _msgSender(), "must be owner");
         MintTokenPrice = price;
     }    
 	
@@ -57,12 +55,6 @@ contract SoulBoundBlessOrBlame is Context, ERC165, IERC1155, IERC1155MetadataURI
         uint16 id16 = (uint16)(id);
         return _balances[id16][account];
     }
-	
-    function ownedOf(address account, uint256 id) external view returns (uint256) {
-        require(account != address(0), "ERC1155: address zero is not a valid owner");
-        uint16 id16 = (uint16)(id);
-        return Owned[account][id16];
-    }	
 
     function balanceOfBatch(
         address[] memory accounts,
@@ -79,96 +71,23 @@ contract SoulBoundBlessOrBlame is Context, ERC165, IERC1155, IERC1155MetadataURI
         return batchBalances;
     }
 
-    function setApprovalForAll(address operator, bool approved) external pure override {
-        //_setApprovalForAll(_msgSender(), operator, approved);
-    }
+    function setApprovalForAll(
+        address ,//operator, 
+        bool //approved
+        ) external pure override {
+       }
 
-    function isApprovedForAll(address account, address operator) external pure override returns (bool) {
-        return false;//_operatorApprovals[account][operator];
+    function isApprovedForAll(
+        address ,//account, 
+        address //operator
+        ) external pure override returns (bool) {
+        return false;
     }
 
     function isIssuerForAll(address issuer, address owner, uint16 id) internal view returns (bool) {
         return _issuer[issuer][owner] == id;
     }    
 
-    /*
-	function _beforeTokenTransfer(
-        address operator,
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) internal {
-        require(ids.length == 1, "one id only per address allowed");
-        require(from == address(0) || (to == address(0) && isIssuerForAll(operator, from, (uint16)(ids[0]))), "This a Soulbound token. It cannot be transferred. It can only be burned by the token issuer.");
-	}
-
-    function safeTransferFrom(
-        address from,
-        address to,
-        uint256 id,
-        uint256 amount,
-        bytes memory data
-    ) public override {
-        require(to != address(0), "ERC1155: transfer to the zero address");
-
-        address operator = _msgSender();
-        uint256[] memory ids = _asSingletonArray(id);
-        uint256[] memory amounts = _asSingletonArray(amount);
-        
-        _beforeTokenTransfer(operator, from, to, ids, amounts, data);
-
-        uint16 id16 = (uint16)(id);
-        uint256 fromBalance = _balances[id16][from];
-        require(fromBalance >= amount, "ERC1155: insufficient balance for transfer");
-        unchecked {
-            _balances[id16][from] = fromBalance - amount;
-        }
-        _balances[id16][to] += amount;
-
-        emit TransferSingle(operator, from, to, id, amount);
-
-        //_afterTokenTransfer(operator, from, to, ids, amounts, data);
-
-        //_doSafeTransferAcceptanceCheck(operator, from, to, id, amount, data);
-    }
-
-
-    function safeBatchTransferFrom(
-        address from,
-        address to,
-        uint256[] memory ids,
-        uint256[] memory amounts,
-        bytes memory data
-    ) public override {
-        require(ids.length == amounts.length, "ERC1155: ids and amounts length mismatch");
-        require(to != address(0), "ERC1155: transfer to the zero address");
-
-        address operator = _msgSender();
-
-        _beforeTokenTransfer(operator, from, to, ids, amounts, data);
-
-        for (uint256 i = 0; i < ids.length; ++i) {
-            uint256 id = ids[i];
-            uint256 amount = amounts[i];
-
-            uint16 id16 = (uint16)(id);
-            uint256 fromBalance = _balances[id16][from];
-            require(fromBalance >= amount, "ERC1155: insufficient balance for transfer");
-            unchecked {
-                _balances[id16][from] = fromBalance - amount;
-            }
-            _balances[id16][to] += amount;
-        }
-
-        emit TransferBatch(operator, from, to, ids, amounts);
-
-        //_afterTokenTransfer(operator, from, to, ids, amounts, data);
-
-        //_doSafeBatchTransferAcceptanceCheck(operator, from, to, ids, amounts, data);
-    }
-    */
 	function _beforeTokenTransfer(
         address operator,
         address from,
@@ -194,10 +113,14 @@ contract SoulBoundBlessOrBlame is Context, ERC165, IERC1155, IERC1155MetadataURI
         bytes memory //data
     ) public pure override { require(false, "not allowed on SBT"); }
 
+    function isContract(address addr) public view returns (bool) {
+        return addr.code.length > 0;
+    }
 
     function mint(address to, uint16 id) external payable {
         require(to != address(0), "ERC1155: mint to the zero address");
         require(id > 0, "min id");
+        require(isContract(to) == false, "vote for the contract creator address instead");
         require(id <= TokenUriLogic.getMaxTokenId(), "max id");
         require(msg.value == MintTokenPrice, "wrong price");
         address operator = _msgSender();
@@ -231,43 +154,18 @@ contract SoulBoundBlessOrBlame is Context, ERC165, IERC1155, IERC1155MetadataURI
         //_afterTokenTransfer(operator, from, address(0), ids, amounts, "");
     }
 
-    /*
-    function burnBatch(address from, uint256[] memory ids, uint256[] memory amounts) external {
-        require(from != address(0), "ERC1155: burn from the zero address");
-        require(ids.length == amounts.length, "ERC1155: ids and amounts length mismatch");
-
-        address operator = _msgSender();
-
-        //_beforeTokenTransfer(operator, from, address(0), ids, amounts, "");
-
-        for (uint256 i = 0; i < ids.length; i++) {
-            uint256 id = ids[i];
-            uint256 amount = amounts[i];
-
-            uint16 id16 = (uint16)(id);
-            uint256 fromBalance = _balances[id16][from];
-            require(fromBalance >= amount, "ERC1155: burn amount exceeds balance");
-            unchecked {
-                _balances[id16][from] = fromBalance - amount;
-            }
-        }
-
-        emit TransferBatch(operator, from, address(0), ids, amounts);
-
-        //_afterTokenTransfer(operator, from, address(0), ids, amounts, "");
-    }
-    */
-
     function setApprovalForAll(address owner, address operator, bool approved) external {
     }
 
+    function withdraw(uint256 amount)
+        external
+        //onlyOwner
+    {
+        uint256 balance = address(this).balance;
+        require(amount < balance);
+        (bool success, ) = payable(RewardContract).call{value: amount}("");
+        require(success, "Failed to send Ether");
+    }    
 
-    /*
-    function _asSingletonArray(uint256 element) private pure returns (uint256[] memory) {
-        uint256[] memory array = new uint256[](1);
-        array[0] = element;
 
-        return array;
-    }
-    */
 }
